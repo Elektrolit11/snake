0e77eb3bc6a8b27640bd5fed14c6aa609df57eb2
Elektrolit11 <kasimob229@yandex.ru>
2018-01-14 01:31:16 +0400

    Создан класс Figure, от которого унаследованы параметры в HorizontalLine и VerticalLine. Создан класс Snake и абстрагированная змейка.

--- Snake/Snake/Snake.cs
+++ Snake/Snake/Snake.cs
@@ -6,17 +6,17 @@ using System.Threading.Tasks;
 
 namespace Snake
 {
-    class VerticalLine : Figure
-    {        
-        public VerticalLine(int yUp, int yDown, int x, char sym)
+    class Snake : Figure
+    {
+        public Snake(Point tail, int length, Direction direction)
         {
             pList = new List<Point>();
-            for (int y = yUp; y <= yDown; y++)
+            for(int i = 0; i < length; i++)
             {
-                Point p = new Point(x, y, sym);
+                Point p = new Point(tail);
+                p.Move(i, direction);
                 pList.Add(p);
             }
         }
-      
     }
 }
